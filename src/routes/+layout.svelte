f85e98ead1aea43b80a2b9834392f212b5d895cf
pablof7z <pfer@me.com>
2023-01-23 18:04:37 +0700

    🍌 on the blockchain

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,6 @@
 <script>
     import "../app.css";
+    const testing = true;
   </script>
   
   <slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,38 @@
+<script>
+    import { page } from '$app/stores';
+
+    let version = 'banana';
+    // let version = PUBLIC_VERSION;
+    let image, title;
+    export let pageTitle;
+
+    // if (!['banana', 'anana'].includes(`${version}`)) {
+        version = $page.url.hostname.match(/bananostr/) ? 'banana' : 'anana';
+    // }
+
+    if (version === 'banana') {
+        image = '/images/banana-svgrepo-com.svg';
+        title = 'BANANOSTR';
+        pageTitle = '🍌 bananostr';
+    } else {
+        image = '/images/pineapple-svgrepo-com.svg';
+        title = 'ANANOSTR';
+        pageTitle = '🍍 ananostr';
+    }
+
+</script>
+
+<svelte:head>
+<title>{pageTitle}</title>
+</svelte:head>
+
 <div class="min-h-screen flex flex-col">
     <div class="flex-grow flex flex-col justify-center items-center">
         <div class="w-64">
-            <img src="/images/pineapple-svgrepo-com.svg">
+            <img src="{image}">
         </div>
         <h1 class="text-4xl md:text-6xl text-purple-800 mt-3" style="font-family: 'Press Start 2P';">
-            ANANOSTR
+            {title}
         </h1>
         <h3 class="text-gray-500 mt-10">
             Nothing to see yet... move along
