25ae1bd15273bf68ac7f3e5412b3fb21df71049c
pablof7z <pfer@me.com>
2023-01-23 13:36:07 +0700

    footer

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,18 @@
-<div class="min-h-screen flex flex-col justify-center items-center">
-    <div class="w-64">
-        <img src="/images/pineapple-svgrepo-com.svg">
+<div class="min-h-screen flex flex-col">
+    <div class="flex-grow flex flex-col justify-center items-center">
+        <div class="w-64">
+            <img src="/images/pineapple-svgrepo-com.svg">
+        </div>
+        <h1 class="text-4xl md:text-6xl text-purple-800" style="font-family: 'Press Start 2P';">
+            ANANOSTR
+        </h1>
+        <h3 class="text-gray-500 mt-10">
+            Nothing to see yet... move along
+        </h3>
     </div>
-    <h1 class="text-6xl text-purple-800" style="font-family: 'Press Start 2P';">
-        ANANOSTR
-    </h1>
-    <h3 class="text-gray-500 mt-10">
-        Nothing to see yet... move along
-    </h3>
+
+    <footer class="p-3 text-center">
+        by
+        <a href="https://pablof7z.com" class="text-purple-900">@pablof7z</a>
+    </footer>
 </div>
